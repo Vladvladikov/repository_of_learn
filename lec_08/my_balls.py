4f848320e88127c21f262868bb6864bb9d08e11a
Vladvladikov <vladvladikov@gmail.com>
2020-04-22 19:04:26 +0300

    Работающая версия шариков с отражением от стен

--- lec_08/my_balls.py
+++ lec_08/my_balls.py
@@ -1,22 +1,26 @@
 import tkinter as tk
 from random import randint
 
-WIDTH = 300
-HEIGHT = 200
+WIDTH = 320
+HEIGHT = 210
 
 def canvas_click_handler(event):
     print('Hello World! x=', event.x, 'y=', event.y)
 
 def tick():
-    global x, y
-    x += 1
-    y += 1
-    canvas.move(ball_id, +1, +1)
+    global x, y, dx, dy
+    x += dx
+    y += dx
+    if x + R > WIDTH or x - R <= 0:
+        dx = - dx
+    if y + R > HEIGHT or y - R <= 0:
+        dy = - dy
+    canvas.move(ball_id, dx, dy)
     root.after(50, tick)
 
 def main():
     global root, canvas
-    global ball_id, x, y, z # TODO: сделать объектно-ориентированный рефакторинг
+    global ball_id, x, y, dx, dy, R # TODO: сделать объектно-ориентированный рефакторинг
     
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
@@ -27,6 +31,7 @@ def main():
     R = randint(20, 50)
     x = randint(R, WIDTH - R)
     y = randint(R, HEIGHT - R)
+    dx, dy = (+ 3, +3)
     ball_id = canvas.create_oval(x - R, y - R, x + R, y + R, fill="green")
 
     tick()
